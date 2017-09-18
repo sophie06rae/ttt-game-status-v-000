3cf3fe259b521d3150c20cd5d3add4cb6a177719
Sophie Rae <sophie.rae.06@gmail.com>
2017-09-18 13:42:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,18 +20,13 @@ def won?(board)
 WIN_COMBINATIONS.each do |win_combination|
 
   win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
 
 
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+
+  if win_index_1
     return win_combination # return the win_combination indexes that won.
-  else position_1 == "O" && position_2 == "O" && position_3 == "0"
-  return win_combination
+  else
+    return false
     end
   end
 end
