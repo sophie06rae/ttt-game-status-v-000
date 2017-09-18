9cfea9bdb5e79a0039cf2d37f6e2eef7a4b2b0e4
Sophie Rae <sophie.rae.06@gmail.com>
2017-09-18 13:12:58 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  
+
   # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
   # grab each index from the win_combination that composes a win.
   win_index_1 = win_combination[0]
@@ -33,5 +33,3 @@ def won?(board)
     false
   end
 end
-
-
