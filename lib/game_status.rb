f01586fe0a08d65b226f7a394c3cd8f2b80537ee
Sophie Rae <sophie.rae.06@gmail.com>
2017-09-18 13:26:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,11 +17,11 @@ WIN_COMBINATIONS = [
 
 def won?(board)
 
-WIN_COMBINATIONS.each do |win_combination|
+WIN_COMBINATIONS.each do |x|
 
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
+  win_index_1 = x[0]
+  win_index_2 = x[1]
+  win_index_3 = x[2]
 
   position_1 = board[win_index_1]
   position_2 = board[win_index_2]
